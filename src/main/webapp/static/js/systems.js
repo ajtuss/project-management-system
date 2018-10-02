04f60b5909de3a7e5667298c3b802e8c0bb7906f
Artur Siembab <ajtuss@gmail.com>
2018-10-02 10:37:48 +0200

    add js for systems page

---
+++ src/main/webapp/static/js/systems.js
@@ -0,0 +1,38 @@
+$(function () {
+
+    var systemsTable = $("#systems");
+
+
+    systemsTable.DataTable({
+        initComplete: function () {
+
+        },
+
+        dom: 'Bfrtip',
+        buttons: [
+            {
+                text: 'Dodaj',
+                action: function (e, dt, button, config) {
+                    window.location.href = '/systems/add';
+                }
+            },
+            {
+                extend: 'selectedSingle',
+                text: 'Edytuj',
+                action: function (e, dt, button, config) {
+                    window.location.href = '/orders/edit?id='+dt.row({selected: true}).data().id;
+                }
+            },
+            {
+                extend: 'selectedSingle',
+                text: 'Usuń',
+                action: function (e, dt, button, config) {
+                    window.location.href = '/orders/delete?id='+dt.row({selected: true}).data().id;
+                }
+            }
+        ],
+        paging: false,
+        select: true
+    });
+
+});
