319be3bb4a1a5a4716a189537e66514befd9d03b
Artur Siembab <ajtuss@gmail.com>
2018-10-02 16:53:29 +0200

    add query for active agreements

--- src/main/java/com/mycompany/repositories/AgreementRepository.java
+++ src/main/java/com/mycompany/repositories/AgreementRepository.java
@@ -2,6 +2,12 @@ package com.mycompany.repositories;
 
 import com.mycompany.domain.Agreement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AgreementRepository extends JpaRepository<Agreement, Long> {
+
+    @Query("SELECT a FROM Agreement a WHERE a.active = TRUE ")
+    List<Agreement> findAllActive();
 }
