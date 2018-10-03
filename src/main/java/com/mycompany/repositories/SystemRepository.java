c02529889303af16d35293464aee3ccc5d90c8b3
Artur Siembab <ajtuss@gmail.com>
2018-10-03 14:54:32 +0200

    add methods

--- src/main/java/com/mycompany/repositories/SystemRepository.java
+++ src/main/java/com/mycompany/repositories/SystemRepository.java
@@ -4,6 +4,13 @@ import com.mycompany.domain.System;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SystemRepository extends JpaRepository<System, Long> {
+
+    Optional<System> findByNameIgnoreCase(String systemName);
+
+    boolean existsByName(String nameValue);
 }
+
