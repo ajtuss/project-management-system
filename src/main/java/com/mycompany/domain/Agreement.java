eecf76fe771f93afed156baf4e2b25961aec670c
Artur Siembab <ajtuss@gmail.com>
2018-10-03 17:01:22 +0200

    delete Lombok

--- src/main/java/com/mycompany/domain/Agreement.java
+++ src/main/java/com/mycompany/domain/Agreement.java
@@ -1,15 +1,13 @@
 package com.mycompany.domain;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name = "agreements")
-@Data
 public class Agreement {
 
     @Id
@@ -40,4 +38,114 @@ public class Agreement {
     @Column(name = "amount_period",length = 10)
     private Period amountPeriod;
     private boolean active;
+
+    public Agreement() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public System getSystem() {
+        return system;
+    }
+
+    public void setSystem(System system) {
+        this.system = system;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(String orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public AmountType getAmountType() {
+        return amountType;
+    }
+
+    public void setAmountType(AmountType amountType) {
+        this.amountType = amountType;
+    }
+
+    public Period getAmountPeriod() {
+        return amountPeriod;
+    }
+
+    public void setAmountPeriod(Period amountPeriod) {
+        this.amountPeriod = amountPeriod;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Agreement agreement = (Agreement) o;
+        return active == agreement.active &&
+                Objects.equals(id, agreement.id) &&
+                Objects.equals(orderNumber, agreement.orderNumber) &&
+                Objects.equals(startDate, agreement.startDate) &&
+                Objects.equals(endDate, agreement.endDate) &&
+                Objects.equals(amount, agreement.amount) &&
+                amountType == agreement.amountType &&
+                amountPeriod == agreement.amountPeriod;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, orderNumber, startDate, endDate, amount, amountType, amountPeriod, active);
+    }
+
+    @Override
+    public String toString() {
+        return "Agreement{" +
+                "id=" + id +
+                ", orderNumber='" + orderNumber + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", amount=" + amount +
+                ", amountType=" + amountType +
+                ", amountPeriod=" + amountPeriod +
+                ", active=" + active +
+                '}';
+    }
 }
