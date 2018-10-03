a4a378e7cc6ef4c890b817898b38f49215694eb5
Artur Siembab <ajtuss@gmail.com>
2018-10-03 10:14:35 +0200

    add multipart config

--- src/main/java/com/mycompany/AppInitializer.java
+++ src/main/java/com/mycompany/AppInitializer.java
@@ -5,8 +5,14 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletRegistration;
+import java.io.File;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    private static final int maxUploadSizeInMb = 5 * 1024 * 1024; // 5 MB
+
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
@@ -23,11 +29,33 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
         return new String[]{"/"};
     }
 
+    /**
+     * Set character encoding filter
+     *
+     * @return
+     */
     @Override
     protected Filter[] getServletFilters() {
         CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
         characterEncodingFilter.setEncoding("UTF-8");
         characterEncodingFilter.setForceEncoding(true);
-        return new Filter[] {characterEncodingFilter};
+        return new Filter[]{characterEncodingFilter};
+    }
+
+    /**
+     * Set multipart configuration for servlet
+     *
+     * @param registration
+     */
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+
+        File uploadDirectory = new File(System.getProperty("java.io.tmpdir"));
+
+        MultipartConfigElement multipartConfigElement =
+                new MultipartConfigElement(uploadDirectory.getAbsolutePath(),
+                        maxUploadSizeInMb, maxUploadSizeInMb, maxUploadSizeInMb);
+
+        registration.setMultipartConfig(multipartConfigElement);
     }
 }
