48c22705d4ffcdaf6bf47483ca899dd7312f67b9
Artur Siembab <ajtuss@gmail.com>
2018-10-03 10:13:39 +0200

    add multipart bean

--- src/main/java/com/mycompany/configuration/AppConfig.java
+++ src/main/java/com/mycompany/configuration/AppConfig.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
@@ -54,5 +56,9 @@ public class AppConfig implements WebMvcConfigurer {
         return new ModelMapper();
     }
 
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
 }
 
