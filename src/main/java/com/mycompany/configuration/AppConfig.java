4ae3782d4b691ad7eb3ada6bc93315cb0dde5030
Artur Siembab <ajtuss@gmail.com>
2018-10-04 14:14:03 +0200

    add beans for include message.properties file with UTF-8

--- src/main/java/com/mycompany/configuration/AppConfig.java
+++ src/main/java/com/mycompany/configuration/AppConfig.java
@@ -1,9 +1,12 @@
 package com.mycompany.configuration;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
@@ -13,8 +16,6 @@ import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
-
-import javax.validation.Validator;
 
 
 @Configuration
@@ -64,9 +65,25 @@ public class AppConfig implements WebMvcConfigurer {
         return new StandardServletMultipartResolver();
     }
 
+
     @Bean
-    public Validator validator() {
-        return new LocalValidatorFactoryBean();
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource bean = new ReloadableResourceBundleMessageSource();
+        bean.setBasename("classpath:messages");
+        bean.setDefaultEncoding("UTF-8");
+        return bean;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+
+    @Override
+    public Validator getValidator() {
+        return validator();
     }
 }
 
