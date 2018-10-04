27da8b748ab131ccc8703cba83668ed1149085b8
Artur Siembab <ajtuss@gmail.com>
2018-10-04 11:52:37 +0200

    add change event on startDate

--- src/main/webapp/static/js/agreement.js
+++ src/main/webapp/static/js/agreement.js
@@ -1,5 +1,11 @@
 $(function () {
 
+    var startDateInput = $('#startDate');
+    var endDateInput = $('#endDate');
 
+    startDateInput.change(function () {
+        endDateInput.prop('min', startDateInput.val());
+    });
+    startDateInput.trigger('change');
 
 });
