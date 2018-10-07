6a20b3d0e68bd04774f4a1598466e25204e4955a
Artur Siembab <ajtuss@gmail.com>
2018-10-07 20:57:09 +0200

    add home test

---
+++ src/main/test/java/com/mycompany/controllers/AppControllerTest.java
@@ -0,0 +1,46 @@
+package com.mycompany.controllers;
+
+import com.mycompany.AppInitializer;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {AppInitializer.class})
+@WebAppConfiguration
+public class AppControllerTest {
+
+
+
+    @InjectMocks
+    private AppController appController;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(appController).build();
+    }
+
+    @Test
+    public void testRootPage() throws Exception {
+        mockMvc.perform(get("/"))
+                                    .andExpect(status().isOk())
+                                    .andExpect(view().name("home"));
+    }
+
+
+}
